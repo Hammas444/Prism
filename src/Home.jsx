889bd1fd759d4cf4191d9cb8f69d8ea06af60b46
M. Hammas Sheikh <113780545+Hammas444@users.noreply.github.com>
2025-06-28 23:32:29 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -112,8 +112,7 @@ const Home = () => {
   <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
     <div className="carousel-inner">
       <div className="carousel-item active">
-{/*         <img src="images/banner.jpg" className="d-block w-100" alt="..." />
-        <div className="carousel-caption  d-md-block" style={{marginBottom: 40}}> */}
+        <div className="carousel-caption  d-md-block" style={{marginBottom: 40}}> 
         </div>
       </div>
     </div>
